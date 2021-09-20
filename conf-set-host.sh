01b96b0f09ff55d66ec65535b201c4effa64d2fd
Chantha VOEURM <45895454+vchantha@users.noreply.github.com>
2021-09-20 16:15:05 +0700

    Add files via upload

---
+++ conf-set-host.sh
@@ -0,0 +1,8 @@
+cat >/etc/hosts<<EOF
+127.0.0.1      localhost
+192.168.56.111 loadbalancer.rean52.com  loadbalancer
+192.168.56.101 kmaster1.rean52.com      kmaster1
+192.168.56.102 kmaster2.rean52.com      kmaster2
+192.168.56.201 kworker1.rean52.com      kworker1
+192.168.56.202 kworker2.rean52.com      kworker2
+EOF
